1ca537e27f6f283751da4545ec45d545811362e9
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2023-09-13 16:03:21 -0400

    added delete route

--- app.js
+++ app.js
@@ -116,6 +116,28 @@ app.post('/blogs', (req, res) => {
         })
 })
 
+app.get('/blogs/:id', (req, res) => {
+    const id = req.params.id
+    Blog.findById(id)
+        .then((result) => {
+            res.render('details', { blog: result, title: 'Blog Details' })
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+})
+
+app.delete('/blogs/:id', (req, res) => {
+    const id = req.params.id
+    Blog.findByIdAndDelete(id)
+        .then((result) => {
+            res.json({ redirect: '/blogs' })
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+})
+
 
 
 // redirects
